98baf0609508ede07d989ce8cbddbabbcd1f560e
AmarnathCJD <amarnathcharichilil@gmail.com>
2026-02-18 19:28:12 +0530

    finish market inteligence page.

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,23 @@ import 'providers/harvest_provider.dart';
 import 'screens/login_screen.dart';
 import 'theme/app_theme.dart';
 
+import 'services/price_alert_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+
+  await PriceAlertService().init();
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => LocationProvider()),
         ChangeNotifierProvider(create: (_) => CropPlanningProvider()),
         ChangeNotifierProvider(create: (_) => HarvestProvider()),
+        ChangeNotifierProvider(
+            create: (_) => PriceAlertService()), // Using the singleton instance
       ],
       child: const AgriTechApp(),
     ),
